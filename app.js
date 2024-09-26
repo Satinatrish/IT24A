b30aa4c7a9ec12a9215b64b23579595d680ec473
Satinatrish <trishaannsatina@gmail.com>
2024-09-26 15:43:20 +0800

    created app.js file and created data types

---
+++ app.js
@@ -0,0 +1,6 @@
+// a. Data Types
+// Using integer, float, string, and boolean data types
+let intNum = 10;       // Integer
+let floatNum = 10.5;   // Float
+let strText = "hello"; // String
+let isValid = true;    // Boolean
