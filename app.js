41c2f778155b8c452d399fc010623c851f4d6d51
Satinatrish <trishaannsatina@gmail.com>
2024-09-26 15:46:44 +0800

    added arithmetic and logical operators in app.js file

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ let intNum = 10;       // Integer
 let floatNum = 10.5;   // Float
 let strText = "hello"; // String
 let isValid = true;    // Boolean
+// b. Arithmetic and Logical Operators
+let sumResult = intNum + floatNum;  // Arithmetic addition
+let difference = intNum - floatNum; // Arithmetic subtraction
+let product = intNum * floatNum;    // Arithmetic multiplication
+let division = intNum / floatNum;   // Arithmetic division
+let isGreater = intNum > floatNum;  // Logical comparison (greater than)
