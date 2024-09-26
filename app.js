ae3075106f60049d8b4fc001403c40282a44bcc2
Satinatrish <trishaannsatina@gmail.com>
2024-09-26 15:48:00 +0800

    added conditional statem

--- app.js
+++ app.js
@@ -10,3 +10,11 @@ let difference = intNum - floatNum; // Arithmetic subtraction
 let product = intNum * floatNum;    // Arithmetic multiplication
 let division = intNum / floatNum;   // Arithmetic division
 let isGreater = intNum > floatNum;  // Logical comparison (greater than)
+
+// c. Conditional Statements
+// i. if-else statement
+if (isGreater) {
+  console.log(`${intNum} is greater than ${floatNum}`);
+} else {
+  console.log(`${intNum} is not greater than ${floatNum}`);
+}
