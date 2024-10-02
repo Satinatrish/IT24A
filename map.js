d17bfb1116b62ca7159d768481f08dc2b03b6a72
Satinatrish <trishaannsatina@gmail.com>
2024-10-02 15:28:52 +0800

    added a loadMarkersFromJson in map.js file

--- map.js
+++ map.js
@@ -14,4 +14,16 @@ initTileLayer() {
         const marker = L.marker([lat, long]).addTo(this.map)
         .bindPopup(message);
     }
-      
+        loadMarkersFromJson(url) {
+            fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(marker => {
+                    this.addMarker(marker.latitude, marker.longitude, marker.message);
+                });
+            })
+            .catch(error => console.error("Error Loading servers:", error));
+        }
+     
+    }
+
