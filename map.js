bb46cc3a10740d734e30e4de3fd2b92d774fb44c
Satinatrish <trishaannsatina@gmail.com>
2024-10-08 14:20:28 +0800

    added object button addeventlistener

--- map.js
+++ map.js
@@ -22,6 +22,11 @@ class leafLetMap{
         this.logCount2Element = document.getElementById('logCountSK');
         this.idContainer = document.getElementById('logContainer');
 
+        this.btn.addEventListener('click', () => this.dataSc());
+        this.btn1.addEventListener('click', () => this.dataLab());
+        this.btn2.addEventListener('click', () => this.dataBa());
+        this.btnclear.addEventListener('click', () => this.clearLogs());
+
 
     }
 
