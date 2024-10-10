2b49fdb9db9abc309198ccc7c1ea82f81dd04d09
Satinatrish <trishaannsatina@gmail.com>
2024-10-11 00:50:14 +0800

    added filterstudents

--- applet 4.js
+++ applet 4.js
@@ -33,9 +33,15 @@ class StudentList {
       studentSearchBar.addEventListener('input', () => {
             this.filterStudents(studentSearchBar.value, studentSearchListContainer);
         });
-        
+
         this.renderStudentList(this.students, studentSearchListContainer);
     }
+
+     filterStudents(query, searchListContainer) {
+        const filteredStudents = this.students.filter(student => {
+            const fullName = `${student.student_name} ${student.student_program}`;
+            return fullName.toLowerCase().includes(query.toLowerCase());
+        });
       }
 
  }
