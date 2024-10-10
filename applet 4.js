6160ad064fdd12d733c69f056e87ea8dd01ef129
Satinatrish <trishaannsatina@gmail.com>
2024-10-11 00:43:43 +0800

    added buttons and parameter

--- applet 4.js
+++ applet 4.js
@@ -17,7 +17,16 @@ class StudentList {
         } catch (error) {
             console.error('Error fetching data:', error);
         }
-    } renderStudentList(students) {
+     renderStudentList(students) {
         const studentListContainer = document.getElementById('studentList');
         studentListContainer.innerHTML = students.map(student => 
+            `<button class="btn btn-primary" style="margin-top:15px; 
+            width:25rem">
+             ${student.student_name} | ${student.student_program}
+      </button><br>`
+   ).join('');
 }
+
+      }
+
+ }
