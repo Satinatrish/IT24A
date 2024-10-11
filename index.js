f08e5b4705a0a85e4ad1256715c4fef5a5b0de53
Satinatrish <trishaannsatina@gmail.com>
2024-10-11 09:57:18 +0800

    added if else statement

--- index.js
+++ index.js
@@ -19,3 +19,12 @@ filterCards(query) {
     this.cards.forEach(card => {
         const title = card.querySelector('.card-title').textContent.toLowerCase();
         const text = card.querySelector('.card-text').textContent.toLowerCase();
+
+        if (title.includes(searchTerm) || text.includes(searchTerm)) {
+            card.closest('.col-sm-4').style.display = '';
+        } else {
+            card.closest('.col-sm-4').style.display = 'none'; 
+        }
+    });
+}
+}
