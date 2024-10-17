4c68bac14ec4df7f65a50e65069d3ccb7830e994
Satinatrish <trishaannsatina@gmail.com>
2024-10-18 00:33:03 +0800

    added object for cityName, temperature,description,humidityand windspeed

--- applet 5.js
+++ applet 5.js
@@ -8,5 +8,11 @@ class WeatherApp {
         this.getLocationBtn = document.getElementById('getLocationBtn');
 
         this.weatherCard = document.getElementById('weatherCard');
+        this.cityName = document.getElementById('cityName');
+        this.temperature = document.getElementById('temperature');
+        this.description = document.getElementById('description');
+        this.humidity = document.getElementById('humidity');
+        this.windSpeed = document.getElementById('windSpeed');
+
     }
 }
