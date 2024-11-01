70aee1557be43456841fbdba139cf2f5cd19e80e
Satinatrish <trishaannsatina@gmail.com>
2024-11-01 20:35:24 +0800

    added method resetEditing

--- applet 6.js
+++ applet 6.js
@@ -62,4 +62,10 @@ class TodoList {
         this.addButton.textContent = 'Update';
     }
 
+    resetEditing() {
+        this.editingIndex = -1;
+        this.addButton.textContent = 'Add';
+    }
+    
 }
+
